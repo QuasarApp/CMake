74385f2ae72c92911d93e75e42349fb32d73f908
EndrII <EndrIIMail@gmail.com>
2021-04-20 11:21:22 +0300

    ref #8 added description into cersion and crossplatform modules

--- crossplatform/crossplatform.cmake
+++ crossplatform/crossplatform.cmake
@@ -13,6 +13,19 @@ endif()
 
 # use TARGET_PLATFORM_TOOLCHAIN
 
+# This module include crossplatform toolchains by target platform.
+# To select target platform set the TARGET_PLATFORM_TOOLCHAIN define.
+# Availabel platforms:
+# * wasm32
+# * win32-g++
+# * win64-g++
+#
+# Note
+# For add full support of the wasm32 build you shold use the initWasmSupport method. This method prepare static build for wasm executable and qt resources.
+#
+# initWasmSupport arguments:
+#  * name - This is name of your initialize targets. You can set it as a CMAKE_PROJECT
+#  * deployFile - This is path to the deploy file of the cqtdepoyer tool. Use it for deploy your site.
 
 set(CROSSPLATFORM_BUILD_TOOLCHAIN_PATH ${CMAKE_CURRENT_LIST_DIR}/${CMAKE_HOST_SYSTEM_NAME}/${TARGET_PLATFORM_TOOLCHAIN}.cmake)
 
