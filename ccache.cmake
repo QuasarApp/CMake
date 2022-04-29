536783fc762c7d382b83601405723c2756e2dfb9
EndrII <EndrIIMail@gmail.com>
2022-04-29 09:27:46 +0300

    added support caching linked libraryes

--- ccache.cmake
+++ ccache.cmake
@@ -18,5 +18,6 @@ find_program(CCACHE_PROGRAM ccache)
 if(CCACHE_PROGRAM)
     message("using ccache in ${CCACHE_PROGRAM}")
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "${CCACHE_PROGRAM}") # Less useful to do it for linking, see edit2
 endif()
 
