77465f8307f749036b288bac3167b8bc596d2796
EndrII <EndrIIMail@gmail.com>
2023-07-21 19:31:37 +0200

    Revert "remove support qt5 for ios"
    This reverts commit da008d0758fa9f1fad59b887c8f29c270332c1a2.

--- QuasarAppCITargets.cmake
+++ QuasarAppCITargets.cmake
@@ -118,6 +118,10 @@ if (ANDROID)
 endif()
 
 if (IOS)
+    if (${QT_VERSION_MAJOR} EQUAL 5)
+        include(${CMAKE_CURRENT_LIST_DIR}/QtStaticCMake/QtStaticCMake.cmake)
+    endif()
+
     include(${CMAKE_CURRENT_LIST_DIR}/QtIosCMake/AddQtIosApp.cmake)
 endif()
 
