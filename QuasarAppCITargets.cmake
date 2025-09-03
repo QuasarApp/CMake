bd6596f2a8f334852511d7d54a27bff7b1685bd2
EndrII <EndrIIMail@gmail.com>
2025-09-04 01:08:40 +0200

    Revert "remove validation"
    This reverts commit 521c4fc2502385b706d17405229875398ada310c.

--- QuasarAppCITargets.cmake
+++ QuasarAppCITargets.cmake
@@ -308,6 +308,11 @@ function(addDeployFromCustomFile name file)
 
     message("cqtdeployer executable: ${CQT_DEPLOYER_EXE}")
 
+    if(NOT EXISTS ${CQT_DEPLOYER_EXE})
+        message("please install the cqtdeployer before deploy this project! ")
+        return()
+    endif(NOT EXISTS ${CQT_DEPLOYER_EXE})
+
 
     if (QT_QMAKE_EXECUTABLE)
         ADD_CUSTOM_TARGET(
